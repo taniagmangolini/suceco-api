f981d3e6f347a1af7f0a76545e3a5ad4c0707598
taniagmangolini <taniagmangolini@gmail.com>
2022-12-30 17:14:36 -0300

    test db

--- app/app/settings.py
+++ app/app/settings.py
@@ -83,9 +83,6 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASS'),
-        'TEST': {
-            'NAME': 'test-database'
-        },
     }
 }
 
