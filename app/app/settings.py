750ec40f160db14464d26d3acc9ed414c1f85aa9
taniagmangolini <taniagmangolini@gmail.com>
2022-12-30 21:07:44 -0300

    test db

--- app/app/settings.py
+++ app/app/settings.py
@@ -76,44 +76,18 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'HOST': os.environ.get('DB_HOST'),
-#        'NAME': os.environ.get('DB_NAME'),
-#        'USER': os.environ.get('DB_USER'),
-#        'PASSWORD': os.environ.get('DB_PASS'),
-#        'TEST': {
-#            'NAME': 'test-database'
-#        },
-#    }
-#}
-
-if 'test' in sys.argv or 'test\_coverage' in sys.argv:
-    DATABASES = {
-        'default': {
-           'ENGINE': 'django.db.backends.postgresql',
-           'NAME': 'github_actions',
-           'USER': 'postgres',
-           'PASSWORD': 'postgres',
-           'HOST': os.environ.get('DB_HOST'),
-           'PORT': '5432',
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+        'TEST': {
+            'NAME': 'postgres'
+        },
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'HOST': os.environ.get('DB_HOST'),
-            'NAME': os.environ.get('DB_NAME'),
-            'USER': os.environ.get('DB_USER'),
-            'PASSWORD': os.environ.get('DB_PASS'),
-            'TEST': {
-                'NAME': 'test-database'
-            },
-        }
-    }
-
+}
 
 # use it if you prefer to use sqlite for tests instead of postgres:
 # if 'test' in sys.argv or 'test\_coverage' in sys.argv: #Covers regular testing and django-coverage
