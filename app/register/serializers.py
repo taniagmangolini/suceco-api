f50bb3bd338f3f32abeb7cbf794f2ccfa46256c9
taniagmangolini <taniagmangolini@gmail.com>
2023-01-15 19:24:49 -0300

    add update and create custom register serializer

--- app/register/serializers.py
+++ app/register/serializers.py
@@ -1,15 +1,22 @@
 """Serializers for Register API."""
 from rest_framework import serializers
 
-from core.models import Register
+from core.models import Register, Forest, Species, Reference
 
 from utils import fields
 from utils.constants import StateType, StageType
 
+from reference.serializers import ReferenceSerializer
+from species.serializers import SpeciesSerializer
+from forest.serializers import ForestSerializer
+
 
 class RegisterSerializer(serializers.ModelSerializer):
-    stage = fields.EnumField(enum=StateType)
-    state = fields.EnumField(enum=StageType)
+    stage = fields.EnumField(enum=StageType)
+    state = fields.EnumField(enum=StateType)
+    reference = ReferenceSerializer(required=True)
+    species = SpeciesSerializer(required=True)
+    forest = ForestSerializer(required=True)
 
     class Meta:
         model = Register
@@ -21,6 +28,56 @@ class RegisterSerializer(serializers.ModelSerializer):
                   'stage',
                   'state']
         read_only_fields = ['id']
+
+    def create(self, validated_data):
+        """Overriding the default create method of the Model \
+        Register serializer to create dependencies forest, species \
+        and reference if necessary.
+        :param validated_data: data containing all the details of a \
+        register
+        :return: returns a successfully created register record."""
+        reference_data = validated_data.pop('reference')
+        reference, created = Reference.objects.get_or_create(**reference_data)
+
+        species_data = validated_data.pop('species')
+        species, created = Species.objects.get_or_create(**species_data)
+
+        forest_data = validated_data.pop('forest')
+        forest, created = Forest.objects.get_or_create(**forest_data)
+
+        register = Register.objects.create(
+            reference=reference,
+            species=species,
+            forest=forest,
+            stage=validated_data.pop('stage'),
+            state=validated_data.pop('state'),
+            latitude=validated_data.pop('latitude', None),
+            longitude=validated_data.pop('longitude', None))
+
+        return register
+
+    def update(self, instance, validated_data):
+        """Overriding the default update method of the Model Register.
+        :param validated_data: data containing all the details of a \
+        register
+        :return: returns a successfully created register record."""
+        ref_data = validated_data.pop('reference', None)
+        if ref_data:
+            reference, created = Reference.objects.get_or_create(**ref_data)
+            instance.reference = reference
+
+        species_data = validated_data.pop('species', None)
+        if species_data:
+            species, created = Species.objects.get_or_create(**species_data)
+            instance.species = species
+
+        forest_data = validated_data.pop('forest', None)
+        if forest_data:
+            forest, created = Forest.objects.get_or_create(**forest_data)
+            instance.forest = forest
+
+        instance.save()
+        return instance
 
 
 class RegisterDetailSerializer(RegisterSerializer):
--- app/register/tests/tests_register_api.py
+++ app/register/tests/tests_register_api.py
@@ -6,7 +6,7 @@ from django.test import Client
 
 from rest_framework import status
 from rest_framework.test import APIClient
-from rest_framework_simplejwt.tokens import AccessToken # noqa
+from rest_framework_simplejwt.tokens import AccessToken
 
 from core.models import Register
 
@@ -15,13 +15,13 @@ from register.serializers import RegisterSerializer, \
 
 from model_bakery import baker
 
-from utils.constants import StateType, StageType
+from utils.constants import StageType, StateType, DomainType
 
 
 CREATE_TOKEN_URL = reverse('token_obtain_pair')
 
 
-REFERENCE_URL = reverse('register:register-list')
+REGISTER_URL = reverse('register:register-list')
 
 
 def detail_url(register_id):
@@ -64,7 +64,7 @@ class PublicRegisterAPITests(TestCase):
     def test_list_register_no_auth_required(self):
         """Test list Register using the api
         when not authenticated."""
-        res = self.api_client.get(REFERENCE_URL)
+        res = self.api_client.get(REGISTER_URL)
         register = Register.objects.filter(is_active=True).order_by('-id')
         serializer = RegisterSerializer(register, many=True)
 
@@ -94,6 +94,14 @@ class PrivateRegisterAPITests(TestCase):
                                            'species': self.species,
                                            'stage': StageType.PIONEIRA,
                                            'state': StateType.SP})
+        self.api_client_admin = APIClient()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@test.com',
+            password='test123555',
+        )
+        admin_token = AccessToken.for_user(user=self.admin_user)
+        autorization = f'Bearer {admin_token}'
+        self.api_client_admin.credentials(HTTP_AUTHORIZATION=autorization)
 
     def test_delete_register_not_authenticated(self):
         """Test delete a register when not authenticated."""
@@ -104,7 +112,7 @@ class PrivateRegisterAPITests(TestCase):
     def test_partial_update_register_not_authenticated(self):
         """Test partial update of a register when not authenticated."""
         payload = {'id': self.register.id,
-                   'stage': StageType.SECUNDARIA}
+                   'stage': StageType.SECUNDARIA.name}
         url = detail_url(self.register.id)
         res = self.no_auth_client.patch(url, payload)
         self.assertEquals(res.status_code, status.HTTP_401_UNAUTHORIZED)
@@ -112,7 +120,51 @@ class PrivateRegisterAPITests(TestCase):
     def test_full_update_register_not_authenticated(self):
         """Test full update of a register when not authenticated."""
         payload = {'id': self.register.id,
-                   'stage': StageType.SECUNDARIA_INICIAL}
+                   'stage': StageType.SECUNDARIA_INICIAL.name}
         url = detail_url(self.register.id)
         res = self.no_auth_client.put(url, payload)
         self.assertEquals(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_register_authenticated(self):
+        """Test create a register when authenticated."""
+        payload = {
+                   'stage': StageType.PIONEIRA.name,
+                   'state': StateType.SP.name,
+                   'forest': {
+                       'name': 'new forest',
+                       'domain': DomainType.MATA_ATLANTICA.name,
+                    },
+                   'reference': {
+                       'publication': 'new pubication test',
+                       'url': 'www.test.com',
+                    },
+                   'species': {
+                       'scientific_name': 'new species test',
+                    },
+                   }
+
+        res = self.api_client_admin.post(REGISTER_URL, payload, format='json')
+        self.assertEquals(res.status_code, status.HTTP_201_CREATED)
+        registers = Register.objects.filter(
+            forest__name=payload['forest']['name'],
+            reference__publication=payload['reference']['publication'],
+            species__scientific_name=payload['species']['scientific_name'],
+            stage=StageType.PIONEIRA,
+            state=StateType.SP,
+        )
+        self.assertEquals(len(registers), 1)
+
+    def test_patch_register_authenticated(self):
+        """Test patch a register when authenticated."""
+        payload = {
+                   'forest': {
+                       'name': 'new forest 2',
+                       'domain': DomainType.MATA_ATLANTICA.name
+                       },
+                   }
+
+        res = self.api_client_admin.patch(detail_url(self.register.id),
+                                          payload, format='json')
+        self.assertEquals(res.status_code, status.HTTP_200_OK)
+        self.register.refresh_from_db()
+        self.assertEquals(self.register.forest.name, payload['forest']['name'])
